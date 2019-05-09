43d2d2df1a4a3e0d37a70b2bfd7eb99afea2c54e
329274634 <329274634@qq.com>
2019-05-09 12:33:31 +0800

    Update Roman numeral to integer

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -7,6 +7,25 @@ namespace LeetCode
         static void Main(string[] args)
         {
             #region 回文数
+            //Console.WriteLine("验证一个数字是否是回文数(按Esc退出,按Enter开始)");
+            //while (true)
+            //{
+            //    ConsoleKeyInfo kinfo = Console.ReadKey();
+            //    if (kinfo.Key == ConsoleKey.Escape)
+            //    {
+            //        break;
+            //    }
+            //    else if (kinfo.Key == ConsoleKey.Enter)
+            //    {
+            //        Console.WriteLine("请输入需要验证的整数:");
+            //        int input = int.Parse(Console.ReadLine());
+            //        bool result = Number.IsPalindrome(input);
+            //        Console.WriteLine("结果是:" + result);
+            //    }
+            //}
+            #endregion
+
+            #region 罗马数字转整数
             Console.WriteLine("验证一个数字是否是回文数(按Esc退出,按Enter开始)");
             while (true)
             {
@@ -17,12 +36,12 @@ namespace LeetCode
                 }
                 else if (kinfo.Key == ConsoleKey.Enter)
                 {
-                    Console.WriteLine("请输入需要验证的整数:");
-                    int resultStr = int.Parse(Console.ReadLine());
-                    bool result = Number.IsPalindrome(resultStr);
+                    Console.WriteLine("请输入需要转换的罗马数:");
+                    string inputString = Console.ReadLine();
+                    int result = Strings.RomanToInt(inputString);
                     Console.WriteLine("结果是:" + result);
                 }
-            } 
+            }
             #endregion
         }
     }
